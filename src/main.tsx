6a68fd31a0d67bb83a6f1b52a3ceda8a1abcbf2f
maksym.kimaiev <kimaiev.maksym@nixsolutions.com>
2024-06-22 02:02:39 +0300

    removed all files

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router/Routes'
-import 'semantic-ui-css/semantic.min.css'
-import 'react-toastify/dist/ReactToastify.min.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router} />
-)
